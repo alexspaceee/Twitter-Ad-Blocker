d774ae84136ce7bf64e2ed1c801d7a8591505470
alexspaceee <102872771+alexspaceee@users.noreply.github.com>
2023-08-03 00:32:43 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,21 +1,24 @@
 function hideTwitterAds() {
-    // Find all spans with the specific class for "Ad"
-    const ads = document.querySelectorAll('span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
+    // Find all spans with the specific class for "Ad", "You Retweeted", etc.
+    const spans = document.querySelectorAll('span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
   
-    ads.forEach((ad) => {
-      // Traverse up the DOM to find the parent Tweet container
-      let tweet = ad;
-      while (tweet && !tweet.getAttribute('data-testid')) {
-        tweet = tweet.parentElement;
-      }
+    spans.forEach((span) => {
+      // Check if the span contains the exact text "Ad"
+      if (span.textContent === 'Ad') {
+        // Traverse up the DOM to find the parent Tweet container
+        let tweet = span;
+        while (tweet && !tweet.getAttribute('data-testid')) {
+          tweet = tweet.parentElement;
+        }
   
-      // If a parent Tweet container is found, hide it
-      if (tweet && tweet.getAttribute('data-testid') === 'tweet') {
-        tweet.style.display = 'none';
+        // If a parent Tweet container is found, hide it
+        if (tweet && tweet.getAttribute('data-testid') === 'tweet') {
+          tweet.style.display = 'none';
+        }
       }
     });
   }
   
   // Run the function regularly to catch new ads as they load
-  setInterval(hideTwitterAds, 1000);
+  setInterval(hideTwitterAds, 1);
   
