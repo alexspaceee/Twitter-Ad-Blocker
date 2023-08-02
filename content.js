b17da026ebc45364da2a6c69b88548b08858bfcb
alexspaceee <102872771+alexspaceee@users.noreply.github.com>
2023-08-03 01:54:37 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,18 +1,21 @@
 function hideTwitterAds() {
     // Find all spans with the specific class for "Ad", "You Retweeted", etc.
     const spans = document.querySelectorAll('span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0');
-  
+    
     spans.forEach((span) => {
       // Check if the span contains the exact text "Ad"
-      if (span.textContent === 'Ad') {
+      if (span.textContent.trim() === 'Ad') {
+        console.log('Ad found:', span);
+        
         // Traverse up the DOM to find the parent Tweet container
         let tweet = span;
         while (tweet && !tweet.getAttribute('data-testid')) {
           tweet = tweet.parentElement;
         }
-  
+    
         // If a parent Tweet container is found, hide it
         if (tweet && tweet.getAttribute('data-testid') === 'tweet') {
+          console.log('Hiding tweet:', tweet);
           tweet.style.display = 'none';
         }
       }
